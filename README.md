a01da2c9a3c21cf07caa15e7276806fa5371b4a8
Torstein Eliassen <torstein.eliassen@gmail.com>
2020-01-03 01:22:54 +0100

    Read how many json files and adapt accordingly

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # Messenger clouds
 
 Make a wordcloud from your facebook messages using wordcloud, pillow, numpy, stop\_words, multidict. Instructions below. 
-**Please contact me for any questions**
+_Please contact me for any questions_
 
 Thanks to [MrGarret45](https://github.com/mrgarrett45/songclouds) for inspiration and for showing how to use the wordcloud library. 
 
 
 ## Getting started
 
-### 1. Fetch your Facebook data
+### 1. Fetch your facebook data
 
-Go to settings on facebook (small triangle in upper right corner) and go to the section: "Your Facebook Information". Press "Download your information" and check only "Messages". Download in JSON format. 
+Go to settings on facebook (small triangle in upper right corner) and go to the section: "Your Facebook Information". 
+Press "Download your information" and download only "Messages". **Download in JSON format**. 
 
 ### 2. Install Python 3+
 
@@ -19,18 +20,18 @@ Python 3 can be installed from [https://www.python.org/downloads/](https://www.p
 
 ### 3. Go for a long walk.
 
-The download of Facebook data takes time, it took me three hours. 
+The download of facebook data takes time, it took me three hours...
 
 ### 4. Clone this repository
 
 ```
 git clone https://github.com/torsteoe/messenger-wordclouds.git
-
 ```
 
 ### 5. Place facebook files in repository directory
 
-Unpack zip-file and choose a chat you want to make a wordcloud out of. Move all files from /messages/inbox/i\[chat\name\] called message\_1.json, message\_2.json etc to the repository directory. In my script I read from 2 json files, this can be reduced or expanded. 
+Unpack zip-file and choose a chat you want to make a wordcloud out of. 
+Move all files from /messages/inbox/i\[chat name\] called message\_1.json, message\_2.json etc to the repository directory. 
 
 ### 6. Install dependencies
 
--- messenger-wc.py
+++ messenger-wc.py
@@ -7,6 +7,7 @@ import numpy as np
 from stop_words import get_stop_words
 import multidict as multidict
 
+
 extra_words = """
 å alle at av både båe bare begge ble blei bli blir blitt då da de deg dei deim deira deires dem den denne der dere deres det dette di din disse ditt du dykk dykkar eg ein eit eitt eller elles en enn er et ett etter før for fordi fra ha hadde han hans har hennar henne hennes her hjå ho hoe honom hoss hossen hun hva hvem hver hvilke hvilken hvis hvor hvordan hvorfor i ikke ikkje ingen ingi inkje inn inni ja jeg kan kom korleis korso kun kunne kva kvar kvarhelst kven kvi kvifor man mange me med medan meg meget mellom men mi min mine mitt mot mykje nå når ned no noe noen noka noko nokon nokor nokre og også om opp oss over på så sånn samme seg selv si sia sidan siden sin sine sitt sjøl skal skulle slik so som somme somt til um upp ut uten vår være vært var vart varte ved vere verte vi vil ville vore vors vort
 """.split()
@@ -28,14 +29,15 @@ def update_table_senders(table, message):
         table[message["sender_name"].split()[0]] = 0
     
 def create_table_from_senders():
-    data1 = get_json_data_from_file("message_1.json")
-    data2 = get_json_data_from_file("message_2.json")
+    data = []
+    for i in range(num_files):
+        data.append(get_json_data_from_file("message_" + str(i+1)+ ".json"))
 
     table =dict()
-    for message in data1["messages"]:
-        update_table_senders(table, message)
-    for message in data2["messages"]:
-        update_table_senders(table, message)
+    for i in range(num_files):
+        for message in data[i]["messages"]:
+            update_table_senders(table, message)
+
     multiDictTable = multidict.MultiDict()
     total, maxFreq, maxName = 0, 0, ""
     for key in table:
@@ -64,14 +66,14 @@ def update_table_content(table, message):
                     table[word] = 0
 
 def create_table_from_content():
-    data1 = get_json_data_from_file("message_1.json")
-    data2 = get_json_data_from_file("message_2.json")
+    data = []
+    for i in range(num_files):
+        data.append(get_json_data_from_file("message_" + str(i+1)+ ".json"))
 
     table =dict()
-    for message in data1["messages"]:
-        update_table_content(table, message)
-    for message in data2["messages"]:
-        update_table_content(table, message)
+    for i in range(num_files):
+        for message in data[i]["messages"]:
+            update_table_content(table, message)
 
     multiDictTable = multidict.MultiDict()
     for key in table:
@@ -85,6 +87,11 @@ def create_wordcloud_freq(table):
     wc.recolor(color_func=color_func)
     wc.to_file(os.path.join(currdir + "generated-wordclouds/", "wordcloud.png"))
 
-create_wordcloud_freq(create_table_from_senders())
+num_files = 0
+for file in os.listdir():
+    if file.endswith(".json"):
+        num_files += 1
 
-#create_wordcloud_freq(create_table_from_content())
+#create_wordcloud_freq(create_table_from_senders())
+
+create_wordcloud_freq(create_table_from_content())
